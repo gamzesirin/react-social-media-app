61a776b469f2103fddbceb806b0f82ef2a574906
Gamze Şirin <gamze1sirin@gmail.com>
2024-01-13 19:05:32 +0300

    context folder created.

---
+++ src/context/darkModeContext.jsx
@@ -0,0 +1,18 @@
+import { createContext } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+export const DarkModeContext = createContext()
+export const DarkModeContextProvider = ({ children }) => {
+	const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem('darkMode')) || false)
+
+	const toggle = () => {
+		setDarkMode(!darkMode)
+	}
+
+	useEffect(() => {
+		localStorage.setItem('darkMode', darkMode)
+	}, [darkMode])
+
+	return <DarkModeContext.Provider value={{ darkMode, toggle }}>{children}</DarkModeContext.Provider>
+}
